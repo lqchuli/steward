d329b2ee7d768c05d99f51b35fe74028d5cd5de7
allan <lqc_huli@163.com>
2017-11-14 20:33:19 +0800

    去掉api的security filter

--- api/src/main/java/com/lingyu/steward/api/SecurityWebApplicationInitializer.java
+++
@@ -1,9 +0,0 @@
-package com.lingyu.steward.api;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-/**
- * @author allan
- */
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
